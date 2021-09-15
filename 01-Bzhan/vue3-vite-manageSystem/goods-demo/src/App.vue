02e460bd1d124f3b5907f74330f47c738711ca8d
lqj1 <11040942848@qq.com>
2021-09-15 18:09:17 +0800

    add

---
+++ 01-Bzhan/vue3-vite-manageSystem/goods-demo/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <GoodsLists></GoodsLists>
+</template>
+
+<script>
+import GoodsLists from './views/GoodsLists.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GoodsLists
+  }
+}
+</script>
