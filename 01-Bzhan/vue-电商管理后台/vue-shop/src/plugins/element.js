3128e950e84896649366f13c8fde8957568de85e
lqj1 <1104942848@qq.com>
2022-01-23 01:06:23 +0800

    Update 完成登录部分功能

--- 01-Bzhan/vue-电商管理后台/vue-shop/src/plugins/element.js
+++ 01-Bzhan/vue-电商管理后台/vue-shop/src/plugins/element.js
@@ -1,4 +1,6 @@
-import Vue from 'vue'
-import { Button } from 'element-ui'
+import Vue from 'vue';
+import { Button } from 'element-ui';
+import { Input } from 'element-ui';
+import { Form, FormItem } from 'element-ui';
 
-Vue.use(Button)
+Vue.use(Button).use(Form).use(FormItem).use(Input);
