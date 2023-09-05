eeac03e92c2cc20fbc624126c89e78cbab3af5e3
lqj1 <1104942848@qq.com>
2023-09-05 15:21:45 +0800

    图片预览-滑块跟随移动

--- 15-vue3电商项目/vue3-rabbit/src/views/Detail/index.vue
+++ 15-vue3电商项目/vue3-rabbit/src/views/Detail/index.vue
@@ -2,12 +2,13 @@
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import DetailHot from './components/DetailHot.vue'
+import ImageView from '@/components/ImageView/index.vue'
 
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
   const res = await getDetail(route.params.id)
-  console.log('res: ', res)
   goods.value = res.result
 }
 onMounted(() => getGoods())
@@ -37,7 +38,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-
+              <ImageView />
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
@@ -120,7 +121,10 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
-
+              <!-- 24小时 -->
+              <DetailHot :hot-type="1" />
+              <!-- 周榜 -->
+              <DetailHot :hot-type="2" />
             </div>
           </div>
         </div>
